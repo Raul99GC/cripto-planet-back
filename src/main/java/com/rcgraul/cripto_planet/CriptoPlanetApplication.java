373f6018b570dd3133f3f702e13cabf42c2b59d3
Raul Cordoba <raulc99g@gmail.com>
2024-10-01 15:26:45 -0600

    creating project

---
+++ src/main/java/com/rcgraul/cripto_planet/CriptoPlanetApplication.java
@@ -0,0 +1,13 @@
+package com.rcgraul.cripto_planet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CriptoPlanetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CriptoPlanetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rcgraul/cripto_planet/CriptoPlanetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rcgraul.cripto_planet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CriptoPlanetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

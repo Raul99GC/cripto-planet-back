8e7d796b27d598f68e31d924993411a452316fce
Raul Cordoba <raulc99g@gmail.com>
2024-11-19 16:29:27 -0600

    Add RoleRepository

--- src/main/java/com/rcgraul/cripto_planet/repositories/RoleRepository.java
+++ src/main/java/com/rcgraul/cripto_planet/repositories/RoleRepository.java
@@ -1,11 +1,15 @@
 package com.rcgraul.cripto_planet.repositories;
 
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.rcgraul.cripto_planet.enums.UserRole;
 import com.rcgraul.cripto_planet.models.Role;
 
 public interface RoleRepository extends JpaRepository<Role, UUID> {
+
+    Optional<Role> findByRoleName(UserRole userRole);
 
 }

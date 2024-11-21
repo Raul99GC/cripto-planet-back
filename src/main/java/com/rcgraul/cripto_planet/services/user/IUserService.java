4733cec425dbc37406c178833806e0e78c3fe81b
Raul99GC <raulc99g@gmail.com>
2024-11-21 17:51:33 -0600

    Add createUser and findByEmail method

--- src/main/java/com/rcgraul/cripto_planet/services/user/IUserService.java
+++ src/main/java/com/rcgraul/cripto_planet/services/user/IUserService.java
@@ -1,9 +1,15 @@
 package com.rcgraul.cripto_planet.services.user;
 
+import java.util.Optional;
+
 import com.rcgraul.cripto_planet.models.User;
 import com.rcgraul.cripto_planet.security.request.SignupRequest;
 
 public interface IUserService {
 
     User registerUser(SignupRequest signupRequest);
+
+    User createUser(User user);
+
+    Optional<User> findByEmail(String email);
 }

5fa42d2ae24d5d6e8d478f86df8255ea0daeb8bf
Raul99GC <raulc99g@gmail.com>
2024-11-21 17:49:45 -0600

    Create UserDetailsServiceImpl class to load user details for authentication

---
+++ src/main/java/com/rcgraul/cripto_planet/security/services/UserDetailsServiceImpl.java
@@ -0,0 +1,40 @@
+package com.rcgraul.cripto_planet.security.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.rcgraul.cripto_planet.models.User;
+import com.rcgraul.cripto_planet.repositories.UserRepository;
+
+/**
+ * Clase `UserDetailsServiceImpl`
+ * <p>
+ * Esta clase implementa la interfaz `UserDetailsService` de Spring Security.
+ * Es responsable de cargar los datos del usuario desde la base de datos (o cualquier otra fuente de datos).
+ * <p>
+ * Funciones principales:
+ * - Buscar un usuario por su email en la base de datos utilizando el `UserRepository`.
+ * - Convertir el usuario encontrado en una instancia de `UserDetailsImpl` para que pueda ser utilizado por Spring Security.
+ * - Lanzar una excepción si no se encuentra el usuario.
+ * <p>
+ * Contexto de uso:
+ * Spring Security invoca esta clase automáticamente durante el proceso de autenticación
+ * para buscar al usuario en la base de datos y verificar sus credenciales.
+ */
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email).orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        return UserDetailsImpl.build(user);
+    }
+
+}

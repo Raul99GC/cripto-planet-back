d8da40f76e3d10f13209de5f524a68ff651a09ba
Raul99GC <raulc99g@gmail.com>
2024-11-24 11:23:28 -0600

    Add OAuth2LoginSuccessHandler to SecurityConfig for handling OAuth2 login success

--- src/main/java/com/rcgraul/cripto_planet/security/SecurityConfig.java
+++ src/main/java/com/rcgraul/cripto_planet/security/SecurityConfig.java
@@ -1,9 +1,11 @@
 package com.rcgraul.cripto_planet.security;
 
+import com.rcgraul.cripto_planet.config.OAuth2LoginSuccessHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -26,6 +28,10 @@ public class SecurityConfig {
     @Autowired
     private AuthEntryPointJwt unauthorizedHandler;
 
+    @Autowired
+    @Lazy
+    private OAuth2LoginSuccessHandler oAuth2LoginSuccessHandler;
+
     @Bean
     public AuthTokenFilter authenticationJwtTokenFilter() {
         return new AuthTokenFilter();
@@ -43,12 +49,16 @@ public class SecurityConfig {
         );
         http.authorizeHttpRequests((requests)
                 -> requests
-                        .requestMatchers("/api/v1/auth/**").permitAll()
-                        .requestMatchers("/api/v1/**").authenticated()
-                        .anyRequest().permitAll()
+                .requestMatchers("/api/v1/auth/**").permitAll()
+                .requestMatchers("/api/v1/**").authenticated()
+                .anyRequest().permitAll()
         );
         http.exceptionHandling(exception -> exception.authenticationEntryPoint(unauthorizedHandler));
         http.addFilterBefore(authenticationJwtTokenFilter(), BasicAuthenticationFilter.class);
+
+        http.oauth2Login(oauth -> {
+            oauth.successHandler(oAuth2LoginSuccessHandler);
+        });
 
         http.csrf(csrf -> csrf.disable());
         http.cors(cors -> cors.configurationSource(corsConfigurationSource()));
@@ -77,6 +87,5 @@ public class SecurityConfig {
             }
         };
     }
-;
 
 }

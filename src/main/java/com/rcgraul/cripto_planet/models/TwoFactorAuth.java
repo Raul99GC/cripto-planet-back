9420e86743d45e68c86e763a4dcb3dcdd16ce356
Raul99GC <raulc99g@gmail.com>
2024-11-24 11:24:57 -0600

    add getter and setter annotation

--- src/main/java/com/rcgraul/cripto_planet/models/TwoFactorAuth.java
+++ src/main/java/com/rcgraul/cripto_planet/models/TwoFactorAuth.java
@@ -3,8 +3,12 @@ package com.rcgraul.cripto_planet.models;
 import com.rcgraul.cripto_planet.enums.VerificationType;
 
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 @Data
+@Setter
+@Getter
 public class TwoFactorAuth {
 
     private boolean isEAbled;

4a994127c59d0a54118f895e06c109deaec67abd
Raul99GC <raulc99g@gmail.com>
2024-11-25 22:36:48 -0600

    Refactor User model to include OAuth support and update signUpMethod handling

--- src/main/java/com/rcgraul/cripto_planet/models/User.java
+++ src/main/java/com/rcgraul/cripto_planet/models/User.java
@@ -1,119 +1,112 @@
 package com.rcgraul.cripto_planet.models;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.UUID;
-
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.rcgraul.cripto_planet.enums.OauthClientId;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 import org.hibernate.annotations.UuidGenerator;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonProperty;
-
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Column;
-import jakarta.persistence.Embedded;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Entity
 @Data
 @NoArgsConstructor
 public class User {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
-    @UuidGenerator
-    private UUID id;
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
+  @UuidGenerator
+  private UUID id;
 
-    private String username;
+  @Column(nullable = false, unique = true)
+  private String username;
 
-    private String firstName;
+  private String firstName;
 
-    private String lastName;
+  private String lastName;
 
-    @Column(nullable = false, unique = true)
-    private String email;
+  private String email;
 
-    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
-    private String password;
+  @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+  private String password;
 
-    private boolean accountNonLocked = true;
-    private boolean accountNonExpired = true;
-    private boolean credentialsNonExpired = true;
-    private boolean enabled = true;
+  private boolean accountNonLocked = true;
+  private boolean accountNonExpired = true;
+  private boolean credentialsNonExpired = true;
+  private boolean enabled = true;
 
-    private LocalDate credentialsExpiryDate;
-    private LocalDate accountExpiryDate;
+  private LocalDate credentialsExpiryDate;
+  private LocalDate accountExpiryDate;
 
-    // private String twoFactorSecret;
-    // private boolean isTwoFactorEnabled = false;
-    @Embedded
-    private TwoFactorAuth twoFactorAuth = new TwoFactorAuth();
-    
-    private String signUpMethod;
+  @Embedded
+  private TwoFactorAuth twoFactorAuth = new TwoFactorAuth();
 
-    @ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.MERGE)
-    @JoinColumn(name = "role_id", referencedColumnName = "id")
-    @JsonBackReference
-    @ToString.Exclude
-    private Role role;
+  // ? seccion de OAuth y de SignUpMethod
+  @Column(nullable = false, columnDefinition = "boolean default false")
+  private boolean isOAuth;
+  @Enumerated(EnumType.STRING)
+  @Column(nullable = true)
+  private OauthClientId signUpMethod;
 
-    @CreationTimestamp
-    @Column(updatable = false)
-    private LocalDateTime createdAt;
+  @ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.MERGE)
+  @JoinColumn(name = "role_id", referencedColumnName = "id")
+  @JsonBackReference
+  @ToString.Exclude
+  private Role role;
 
-    @UpdateTimestamp
-    private LocalDateTime updatedAt;
+  @CreationTimestamp
+  @Column(updatable = false)
+  private LocalDateTime createdAt;
 
-    public User(String userName, String email, String password) {
-        this.username = userName;
-        this.email = email;
-        this.password = password;
+  @UpdateTimestamp
+  private LocalDateTime updatedAt;
+
+  public User(String userName, String email, String password) {
+    this.username = userName;
+    this.email = email;
+    this.password = password;
+  }
+
+  public User(String userName, String email) {
+    this.username = userName;
+    this.email = email;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj) {
+      return true;
     }
-
-    public User(String userName, String email) {
-        this.username = userName;
-        this.email = email;
+    if (obj == null) {
+      return false;
     }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (obj == null) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-        User other = (User) obj;
-        if (id == null) {
-            if (other.id != null) {
-                return false;
-            }
-        } else if (!id.equals(other.id)) {
-            return false;
-        }
-        return true;
+    if (getClass() != obj.getClass()) {
+      return false;
     }
-
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((id == null) ? 0 : id.hashCode());
-        return result;
+    User other = (User) obj;
+    if (id == null) {
+      if (other.id != null) {
+        return false;
+      }
+    } else if (!id.equals(other.id)) {
+      return false;
     }
+    return true;
+  }
+
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + ((id == null) ? 0 : id.hashCode());
+    return result;
+  }
 
 }

22e5831649292d4aeaefe44eaaf99334b01eddc0
Raul99GC <raulc99g@gmail.com>
2024-11-25 22:53:05 -0600

    add forgot-password endpoint to the auth controller

--- src/main/java/com/rcgraul/cripto_planet/controllers/AuthController.java
+++ src/main/java/com/rcgraul/cripto_planet/controllers/AuthController.java
@@ -1,21 +1,5 @@
 package com.rcgraul.cripto_planet.controllers;
 
-import com.rcgraul.cripto_planet.security.services.UserDetailsImpl;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.rcgraul.cripto_planet.exceptions.EmailAlreadyExistsException;
 import com.rcgraul.cripto_planet.exceptions.UsernameAlreadyExistsException;
 import com.rcgraul.cripto_planet.security.MessageResponse;
@@ -23,10 +7,20 @@ import com.rcgraul.cripto_planet.security.jwt.JwtUtils;
 import com.rcgraul.cripto_planet.security.request.SigninRequest;
 import com.rcgraul.cripto_planet.security.request.SignupRequest;
 import com.rcgraul.cripto_planet.security.response.AuthResponse;
+import com.rcgraul.cripto_planet.security.services.UserDetailsImpl;
 import com.rcgraul.cripto_planet.security.services.UserDetailsServiceImpl;
 import com.rcgraul.cripto_planet.services.user.UserService;
-
+import jakarta.mail.MessagingException;
 import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/auth")
@@ -81,8 +75,7 @@ public class AuthController {
         try {
             authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(userReq.getEmail(), userReq.getPassword()));
         } catch (AuthenticationException e) {
-            System.out.println(e);
-            throw new BadCredentialsException("Invalid credentials", e);
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(new MessageResponse("Invalid credentials"));
         }
 
         // set the authentication
@@ -101,5 +94,23 @@ public class AuthController {
         return new ResponseEntity<>(res, HttpStatus.ACCEPTED);
 
     }
+
+    @PostMapping("/forgot-password")
+    public ResponseEntity<?> forgotPassword(@RequestParam String email) {
+
+        try {
+            userService.generatePasswordResetToken(email);
+            return ResponseEntity.ok(new MessageResponse("password reset email sent"));
+        } catch (MessagingException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new MessageResponse("Error sending password reset email"));
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(new MessageResponse(e.getMessage()));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new MessageResponse(e.getMessage()));
+        }
+    }
+
+    
+
 
 }

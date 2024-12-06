dfe06ce7669286e53bf5e59802bd46443f0ffc48
Raul99GC <raulc99g@gmail.com>
2024-12-06 09:51:14 -0600

    Add custom query to UserRepository for finding user by username or email

--- src/main/java/com/rcgraul/cripto_planet/repositories/UserRepository.java
+++ src/main/java/com/rcgraul/cripto_planet/repositories/UserRepository.java
@@ -2,6 +2,7 @@ package com.rcgraul.cripto_planet.repositories;
 
 import com.rcgraul.cripto_planet.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 import java.util.UUID;
@@ -15,4 +16,8 @@ public interface UserRepository extends JpaRepository<User, UUID> {
     public boolean existsByUsername(String username);
 
     Optional<User> findByUsername(String username);
+
+    @Query("select u from User u where u.username = ?1 or u.email = ?1")
+    Optional<User> findByUsernameOrEmail(String usernameOrEmail);
+
 }

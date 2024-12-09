4e0207ef531248a5983e5d6e6e6190425c2ba21d
Raul99GC <raulc99g@gmail.com>
2024-12-09 16:33:31 -0600

    Add Order entity

---
+++ src/main/java/com/rcgraul/cripto_planet/models/Order.java
@@ -0,0 +1,45 @@
+package com.rcgraul.cripto_planet.models;
+
+import com.rcgraul.cripto_planet.enums.OrderStatus;
+import com.rcgraul.cripto_planet.enums.OrderType;
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@Entity
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @ManyToOne
+    private User user;
+
+    @Column(nullable = false)
+    private OrderType orderType;
+
+    @Column(nullable = false)
+    private BigDecimal price;
+
+    @Column(nullable = false)
+    private OrderStatus status;
+
+    @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+    private OrderItem orderItem;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+
+}

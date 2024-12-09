d05e2b4454381aa4c8198fab1c7b28267df24e5b
Raul99GC <raulc99g@gmail.com>
2024-12-09 16:25:35 -0600

    Add CascadeType.ALL to PasswordResetToken's user relationship

--- src/main/java/com/rcgraul/cripto_planet/models/PasswordResetToken.java
+++ src/main/java/com/rcgraul/cripto_planet/models/PasswordResetToken.java
@@ -24,7 +24,7 @@ public class PasswordResetToken {
 
     private boolean used;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
 

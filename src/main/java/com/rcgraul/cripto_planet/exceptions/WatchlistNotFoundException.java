71c69b9bbaf2bc0e8b0bb493033f87430f46c396
Raul99GC <raulc99g@gmail.com>
2024-12-24 14:32:07 -0600

    Add WatchlistNotFoun Exception

---
+++ src/main/java/com/rcgraul/cripto_planet/exceptions/WatchlistNotFoundException.java
@@ -0,0 +1,9 @@
+package com.rcgraul.cripto_planet.exceptions;
+
+public class WatchlistNotFoundException extends RuntimeException {
+
+    public WatchlistNotFoundException(String message) {
+        super(message);
+    }
+
+}

99b50bb1144554d2a0f29bcfa5fa4b147a5b20cf
Raul99GC <raulc99g@gmail.com>
2024-12-24 13:43:15 -0600

    Setting ZERO as default on balance and add createdAt and updatedAt

--- src/main/java/com/rcgraul/cripto_planet/models/Wallet.java
+++ src/main/java/com/rcgraul/cripto_planet/models/Wallet.java
@@ -2,8 +2,11 @@ package com.rcgraul.cripto_planet.models;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Entity
@@ -17,5 +20,12 @@ public class Wallet {
     @OneToOne
     private User user;
 
-    private BigDecimal balance;
+    private BigDecimal balance = BigDecimal.ZERO;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
 }
